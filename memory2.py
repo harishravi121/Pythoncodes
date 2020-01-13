3cd65bf9a627e45675309f62f3e88f4fc3a4d794
Harish Mckelwyn Ravi <39822028+harishravi121@users.noreply.github.com>
2020-01-13 11:56:42 +0530

    Add files via upload

---
+++ memory2.py
@@ -0,0 +1,26 @@
+a = 2
+import random
+
+n = 0
+ha = 1
+import time
+
+x = 0
+
+print('hello world')
+while ha == 1:
+
+    b = random.sample((1, 2, 4, 3, 5, 6,7,8,9), 2)
+
+    s=b[0] + 10 * b[1]
+    print(s)
+    time.sleep(3)
+    for i in range(1,20):
+        print()
+    a=int(input())
+
+    if (s == a):
+        print('congrats')
+
+
+
