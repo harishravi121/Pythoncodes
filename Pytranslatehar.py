a7d406b9975d432dbf501dfd09a430e8ac4106f7
Harish Ravi <39822028+harishravi121@users.noreply.github.com>
2022-11-15 12:51:14 +0530

    Add files via upload

---
+++ Pytranslatehar.py
@@ -0,0 +1,33 @@
+English='''Hi how are you. Aesop's Fables, or the Aesopica, is a collection of fables credited to Aesop, a slave and storyteller believed to have lived in ancient Greece between 620 and 564 BCE. Of diverse origins, the stories associated with his name have descended to modern times through a number of sources and continue to be reinterpreted in different verbal registers and in popular as well as artistic media.
+
+The fables originally belonged to oral tradition and were not collected for some three centuries after Aesop's death. By that time, a variety of other stories, jokes and proverbs were being ascribed to him, although some of that material was from sources earlier than him or came from beyond the Greek cultural sphere. The process of inclusion has continued until the present, with some of the fables unrecorded before the Late Middle Ages and others arriving from outside Europe. The process is continuous and new stories are still being added to the Aesop corpus, even when they are demonstrably more recent work and sometimes from known authors.
+
+Manuscripts in Latin and Greek were important avenues of transmission, although poetical treatments in European vernaculars eventually formed another. On the arrival of printing, collections of Aesop's fables were among the earliest books in a variety of languages. Through the means of later collections, and translations or adaptations of them, Aesop's reputation as a fabulist was transmitted throughout the world.
+
+Initially the fables were addressed to adults and covered religious, social and political themes. They were also put to use as ethical guides and from the Renaissance onwards were particularly used for the education of children. Their ethical dimension was reinforced in the adult world through depiction in sculpture, painting and other illustrative means, as well as adaptation to drama and song. In addition, there have been reinterpretations of the meaning of fables and changes in emphasis over time.'''
+Tamil='''ஹாய் எப்படி இருக்கீங்க. ஈசோப்பின் கட்டுக்கதைகள், அல்லது ஈசோபிகா, கிமு 620 மற்றும் 564 க்கு இடையில் பண்டைய கிரேக்கத்தில் வாழ்ந்ததாக நம்பப்படும் அடிமை மற்றும் கதைசொல்லி ஈசோப்புக்கு வரவு வைக்கப்பட்ட கட்டுக்கதைகளின் தொகுப்பாகும். பல்வேறு தோற்றம் கொண்ட, அவரது பெயருடன் தொடர்புடைய கதைகள் பல ஆதாரங்கள் மூலம் நவீன காலத்திற்கு வந்துள்ளன, மேலும் வெவ்வேறு வாய்மொழி பதிவேடுகள் மற்றும் பிரபலமான மற்றும் கலை ஊடகங்களில் தொடர்ந்து மறுபரிசீலனை செய்யப்படுகின்றன.
+
+கட்டுக்கதைகள் முதலில் வாய்வழி பாரம்பரியத்தைச் சேர்ந்தவை மற்றும் ஈசோப்பின் மரணத்திற்குப் பிறகு சுமார் மூன்று நூற்றாண்டுகளுக்கு சேகரிக்கப்படவில்லை. அந்த நேரத்தில், பலவிதமான கதைகள், நகைச்சுவைகள் மற்றும் பழமொழிகள் அவருக்குக் கூறப்பட்டன, இருப்பினும் அவற்றில் சில அவரை விட முந்தைய மூலங்களிலிருந்து வந்தவை அல்லது கிரேக்க கலாச்சாரக் கோளத்திற்கு அப்பாற்பட்டவை. இடைக்காலத்தின் பிற்பகுதிக்கு முன் பதிவு செய்யப்படாத சில கட்டுக்கதைகள் மற்றும் மற்றவை ஐரோப்பாவிற்கு வெளியில் இருந்து வந்துசேர்க்கும் செயல்முறை தற்போது வரை தொடர்கிறது. இந்த செயல்முறை தொடர்ச்சியாக உள்ளது மற்றும் புதிய கதைகள் ஈசோப் கார்பஸில் இன்னும் சேர்க்கப்படுகின்றன, அவை மிகவும் சமீபத்திய படைப்புகளாக இருந்தாலும் சில சமயங்களில் அறியப்பட்ட ஆசிரியர்களிடமிருந்தும் கூட.
+
+லத்தீன் மற்றும் கிரேக்க மொழிகளில் கையெழுத்துப் பிரதிகள் பரிமாற்றத்தின் முக்கிய வழிகளாக இருந்தன, இருப்பினும் ஐரோப்பிய வடமொழிகளில் கவிதை சிகிச்சைகள் இறுதியில் மற்றொன்றை உருவாக்கியது. அச்சிடலின் வருகையில், ஈசோப்பின் கட்டுக்கதைகளின் தொகுப்புகள் பல்வேறு மொழிகளில் ஆரம்பகால புத்தகங்களில் ஒன்றாக இருந்தன. பிற்கால சேகரிப்புகள் மற்றும் அவற்றின் மொழிபெயர்ப்புகள் அல்லது தழுவல்கள் மூலம், ஈசோப்பின் ஒரு கற்பனைவாதி என்ற புகழ் உலகம் முழுவதும் பரவியது.
+
+ஆரம்பத்தில் கட்டுக்கதைகள் பெரியவர்களுக்கு உரையாற்றப்பட்டது மற்றும் மத, சமூக மற்றும் அரசியல் கருப்பொருள்களை உள்ளடக்கியது. அவர்கள் நெறிமுறை வழிகாட்டிகளாகவும் பயன்படுத்தப்பட்டனர் மற்றும் மறுமலர்ச்சியில் இருந்து குறிப்பாக குழந்தைகளின் கல்விக்காக பயன்படுத்தப்பட்டனர். அவர்களின் நெறிமுறை பரிமாணம் வயது வந்தோருக்கான உலகில் சிற்பம், ஓவியம் மற்றும் பிற விளக்க வழிமுறைகள் மற்றும் நாடகம் மற்றும் பாடலுக்குத் தழுவல் மூலம் வலுப்படுத்தப்பட்டது. கூடுதலாக, கட்டுக்கதைகளின் அர்த்தத்தின் மறுவிளக்கம் மற்றும் காலப்போக்கில் வலியுறுத்தலில் மாற்றங்கள் உள்ளன.'''
+
+
+English_sentances=English.split('.')
+Tamil_sentances=Tamil.split('.')
+
+Totranslate='Hi how are you'
+Answer=''
+flag=0
+for i in range(len(English_sentances)):
+    if(flag==0):
+        if(Totranslate==English_sentances[i]):
+            Answer=Tamil_sentances[i]
+            flag=1
+
+        else:
+            Answer='Not there'
+
+
+print(Answer)
