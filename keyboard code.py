9b1aaed278bc3ee979f00d3b29eaee6fafa0b3b2
Harish Ravi <39822028+harishravi121@users.noreply.github.com>
2024-08-23 05:52:35 +0530

    Add files via upload

---
+++ keyboard code.py
@@ -0,0 +1,13 @@
+interrupt pin 1:
+    k1=1.3
+    k2=2.9
+
+interrupt pin 2:
+    k1 = 1.9
+    k3=4
+
+
+while 1:
+    if(pin3=1):
+        for(t in range(200)):
+            pin3=DAC(5sint(k1wt)+5sint(k2wt))
