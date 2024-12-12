68adb816702398f385f15173f70da39ccd6700e5
Harish Ravi <39822028+harishravi121@users.noreply.github.com>
2024-12-12 10:14:36 +0530

    Update and rename twofadevices to twofadevices.py

--- twofadevices.py
+++ twofadevices.py
@@ -1,10 +1,15 @@
+
+
 c=5+random.randint(1,5)
+
 if(c==5+random.randint(1,5)):
     a=input()
     b=input()
     if((a=='Password1')&&(b=='Password2')):
         print('THe OS is open')
+
 else:
     a=input()
+
     if((a=='Password1')):
-        print('THe OS is open
+        print('THe OS is open')
